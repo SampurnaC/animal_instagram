ae7ae2624f3cfe1f9098559be5e7a8c3ec27f567
sam <sampurna.chapagainnn@gmail.com>
2022-09-24 21:48:34 +0100

    work on index page view to list all values

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def posts_list(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'post/list.html', context)
